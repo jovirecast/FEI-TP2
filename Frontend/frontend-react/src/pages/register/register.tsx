1349006320ed2db1a77edbc2f78f19d8ce1c0619
jovirecast <jreyescastello@gmail.com>
2025-10-08 19:24:50 -0300

    fix: modificación en trivia y comienzo de módulo de registro/validación

--- Frontend/frontend-react/src/pages/register/register.tsx
+++ Frontend/frontend-react/src/pages/register/register.tsx
@@ -1,6 +1,127 @@
+import { useState } from "react";
+
 export default function Register() {
-  return(
+  const strapiUrl = import.meta.env.VITE_API_STRAPI;
+
+  const [formData, setFormData] = useState({
+    nickname: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch(strapiUrl + "/usuarios", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ data: formData }),
+      });
+
+      if (!res.ok) throw new Error("Error al enviar el formulario");
+
+      const data = await res.json();
+      console.log("Guardado en Strapi:", data);
+
+      alert("Te registraste exitosamente ✅");
+      setFormData({ nickname: "", email: "", password: ""});
+    } catch (error) {
+      console.error(error);
+      alert("Hubo un error en tu registro ❌");
+    }
+  };
+  return (
     <>
+      {/* <!-- Section 5: Contact --> */}
+      <section className="contact" id="contact">
+        <div className="hero-content contact-container ">
+          <form className="contact-form glass" onSubmit={handleSubmit}>
+            <div className="text-center contact-info">
+              <h3 className="text-[20px]">
+                ¿Querés ser parte de nuestros usuarios?
+              </h3>
+            </div>
+            <div className="p-4">
+              <div className="form-group">
+                <input
+                  type="text"
+                  placeholder="Usuario"
+                  required
+                  id="nickname"
+                  value={formData.nickname}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="email"
+                  placeholder="Correo electrónico"
+                  required
+                  id="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  placeholder="Contraseña"
+                  required
+                  id="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            <button type="submit" className="submit-btn">
+              Registrate
+            </button>
+          </form>
+
+          <div className="hero-content contact-container">
+            {/* <form className="contact-form glass" onSubmit={handleSubmit}>
+              <div className="text-center contact-info">
+                <h3 className="text-[20px]">¿Ya te registraste?</h3>
+                <p>Validá tus credenciales</p>
+              </div>
+              <div className="p-4">
+                <div className="form-group">
+                  <input
+                    type="text"
+                    placeholder="Usuario"
+                    required
+                    id="nickname"
+                    value={formData.nickname}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    placeholder="Contraseña"
+                    required
+                    id="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+              <button type="submit" className="submit-btn">
+                Verificá tu usuario
+              </button>
+            </form> */}
+          </div>
+        </div>
+      </section>
     </>
   );
 }
--- Frontend/frontend-react/src/pages/trivia/trivia.tsx
+++ Frontend/frontend-react/src/pages/trivia/trivia.tsx
@@ -20,27 +20,40 @@ interface Pregunta {
   respuestas: Respuesta[];
 }
 
+/* 
+* IMPORTANTE:
+* Este componente funciona correctamente si ya existe una pregunta cargada en la base de datos consultada.
+* Si no es así, se puede crear una pregunta manualmente en el CMS Strapi.
+*/
 export default function Trivia() {
   const API_STRAPI = import.meta.env.VITE_API_STRAPI;
 
   const [pregunta, setPregunta] = useState<Pregunta | null>(null);
-  const [respuestaSeleccionada, setRespuestaSeleccionada] = useState<Respuesta | null>(null);
+  const [respuestaSeleccionada, setRespuestaSeleccionada] =
+    useState<Respuesta | null>(null);
   const [resultado, setResultado] = useState("");
 
   // Cargar una pregunta al iniciar
   useEffect(() => {
-    traerPregunta(4);
+    traerPreguntaAleatoria();
   }, []);
 
-  const traerPregunta = async (id: number) => {
+  const traerPreguntaAleatoria = async () => {
+    setRespuestaSeleccionada(null);
+    setResultado("");
     try {
-      const res = await fetch(`${API_STRAPI}/preguntas?filters[id][$eq]=${id}&populate=respuestas`);
-      if (!res.ok) throw new Error("Error al traer la pregunta");
+      const res = await fetch(`${API_STRAPI}/preguntas?populate=respuestas`);
+      if (!res.ok) throw new Error("Error al traer preguntas");
 
       const data = await res.json();
-      // { data: { id: 1, attributes: { texto: "..." } } }
-      const pregunta = data.data[0] || "Pregunta no encontrada";
-      setPregunta(pregunta); // tu estado React
+      if (data.data.length === 0) {
+        setPregunta(null);
+        return;
+      }
+
+      const randomIndex = Math.floor(Math.random() * data.data.length);
+      const pregunta = data.data[randomIndex];
+      setPregunta(pregunta);
     } catch (error) {
       console.error("Error al traer pregunta:", error);
     }
@@ -52,46 +65,55 @@ export default function Trivia() {
     else setResultado("❌ Incorrecto.");
   };
 
-  if (!pregunta) return <section className="hero" id="home"><div className="hero-content"><p className="text-center"> Cargando pregunta...</p></div></section>;
+  if (!pregunta)
+    return (
+      <section className="hero" id="home">
+        <div className="hero-content">
+          <p className="text-center"> Cargando pregunta...</p>
+        </div>
+      </section>
+    );
   return (
     <>
       <section className="hero" id="home">
         <div className="hero-content p-6">
-          <div className="flex flex-col glass gap-3">
-              <h2 className="text-xl font-semibold mb-4 text-center">
-                {pregunta.texto}
-              </h2>
+          <div className="flex flex-col glass gap-3 m-4">
+            <h2 className="text-xl font-semibold mb-4 text-center">
+              {pregunta.texto}
+            </h2>
 
-              <div className="flex flex-col items-center gap-4">
-                {pregunta.respuestas.map((r, i) => (
-                  <button
-                    key={i}
-                    onClick={() => seleccionarRespuesta(r)}
-                    className={`w-1/2 p-3 rounded-lg border transition ${respuestaSeleccionada?.texto === r.texto
+            <div className="flex flex-col items-center gap-4">
+              {pregunta.respuestas.map((r, i) => (
+                <button
+                  key={i}
+                  onClick={() => seleccionarRespuesta(r)}
+                   disabled={!!respuestaSeleccionada}
+                  className={`w-1/2 p-3 rounded-lg border transition ${
+                    respuestaSeleccionada?.texto === r.texto
                       ? r.es_correcta
                         ? "bg-green-100 border-green-500"
                         : "bg-red-100 border-red-500"
-                      : "hover:bg-gray-100 border-gray-300"
-                      }`}
-                  >
-                    {r.texto}
-                  </button>
-                ))}
-              </div>
-
-              {resultado && (
-                <p className="mt-4 text-center font-medium">
-                  {resultado}
-                </p>
-              )}
-
-              <div className="text-center mt-6">
-                <button
-                  className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg"
+                      : "hover:bg-gray-100 border-gray-300"}
+                      ${respuestaSeleccionada ? "cursor-not-allowed opacity-70" : ""}
+                  `}
                 >
-                  Siguiente pregunta
+                  {r.texto}
                 </button>
-              </div>
+              ))}
+            </div>
+
+            {resultado && (
+              <p className="mt-4 text-center font-medium">{resultado}</p>
+            )}
+
+            <div className="text-center mt-6">
+              <button
+                onClick={traerPreguntaAleatoria}
+                className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg"
+              >
+                Nueva pregunta aleatoria
+              </button>
+            </div>
           </div>
         </div>
       </section>
